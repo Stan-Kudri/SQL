b40bfe409c3b1c0c55daac8a9aefdbfd2298282b
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-01-31 00:25:11 +0300

    Create NinthTask.sql

---
+++ Task/NinthTask.sql
@@ -0,0 +1,7 @@
+--9. Вывести имена и ID пяти самых покупаемых продуктов. А также сколько их было продано.
+SELECT ProductName, Products.ProductID, SUM(Quantity) AS SumQuantity 
+  FROM Products, OrderDetails
+	WHERE OrderDetails.ProductID = Products.ProductID
+GROUP BY ProductName 
+ORDER BY SUM(Quantity) DESC
+LIMIT 5
