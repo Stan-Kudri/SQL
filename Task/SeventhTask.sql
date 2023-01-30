e96350c003df7123051f12d41c30d43b723bb143
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-01-30 21:53:46 +0300

    Update SeventhTask.sql

--- Task/SeventhTask.sql
+++ Task/SeventhTask.sql
@@ -1,3 +1,6 @@
 --7. Вывести имена всех людей, которые покупали морские продукты.
-DELETE FROM [Categories]
-WHERE Description LIKE '%Sea%' OR '%fish%';
+SELECT DISTINCT CustomerName FROM Customers
+JOIN Orders on Orders.CustomerID = Customers.CustomerID
+JOIN OrderDetails on OrderDetails.OrderID = Orders.OrderID
+JOIN Products on Products.ProductID = OrderDetails.ProductID
+WHERE Products.CategoryID = 8
