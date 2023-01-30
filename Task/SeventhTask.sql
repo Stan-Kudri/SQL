018f3fabceca47405250b15a8249c63be0adfd97
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-01-30 22:15:13 +0300

    Update SeventhTask.sql

--- Task/SeventhTask.sql
+++ Task/SeventhTask.sql
@@ -1,6 +1,6 @@
 --7. Вывести имена всех людей, которые покупали морские продукты.
 SELECT DISTINCT CustomerName FROM Customers
-JOIN Orders on Orders.CustomerID = Customers.CustomerID
-JOIN OrderDetails on OrderDetails.OrderID = Orders.OrderID
-JOIN Products on Products.ProductID = OrderDetails.ProductID
+  INNER JOIN Orders on Orders.CustomerID = Customers.CustomerID
+  INNER JOIN OrderDetails on OrderDetails.OrderID = Orders.OrderID
+  INNER JOIN Products on Products.ProductID = OrderDetails.ProductID
 WHERE Products.CategoryID = 8
