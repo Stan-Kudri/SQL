55c9ea81d6307eccdda716de19248ca82e1e1da5
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-01-31 22:47:37 +0300

    Update TenthTask.sql

--- Task/TenthTask.sql
+++ Task/TenthTask.sql
@@ -1,5 +1,5 @@
 --10. Покажите сколько заказов было сделано каждым грузоотправителем. Отобразите имена грузоотправителей и количество выполненных ими заказов.
-SELECT Shippers.ShipperID, Shippers.ShipperName, SUM(Quantity) AS AmountOfShipments FROM Shippers, OrderDetails
+SELECT Shippers.ShipperID, Shippers.ShipperName, SUM(Quantity) AS AmountOfShipments FROM Shippers
 	INNER JOIN Orders ON Orders.ShipperID = Shippers.ShipperID
-	WHERE OrderDetails.OrderID = Orders.OrderID
+	INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
 GROUP BY Shippers.ShipperID
