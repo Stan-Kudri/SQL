c13953b4946883eadecc41c8947582c626e36a83
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-02-01 00:00:23 +0300

    Update EighthTask.sql

--- Task/EighthTask.sql
+++ Task/EighthTask.sql
@@ -1,5 +1,7 @@
 -- 8. Вывести имя клиента и количество совершенных им покупок.
-SELECT ContactName, SUM(Quantity) AS SumQuantity
+SELECT
+	ContactName,
+	SUM(Quantity) AS SumQuantity
 FROM Customers
 	INNER JOIN Orders ON Orders.CustomerID = Customers.CustomerID
 	INNER JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
